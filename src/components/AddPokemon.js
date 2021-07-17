6ffc4fa5885473cfcdf49af73482c5ca002f1c4d
Brian Vanegas <brianvs18@gmail.com>
2021-07-16 20:37:26 -0500

    secound commit

--- src/components/AddPokemon.js
+++ src/components/AddPokemon.js
@@ -10,24 +10,11 @@ const AddPokemon = (props) => {
         try {
             let config = {
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
+                    'Content-Type': 'application/json'
                 }
               }
               let data = JSON.stringify(newPokemon)
-           let response= await axios.post("https://pokemon-api-node.herokuapp.com/poke-api/v1/pokemon",{name:"buenosdias",hp:54,damage:50,defence:80,image:"sdfkosmdsdfsdfklfmsdklfm",elementtype:"dsfsdfsdfdfsdfsd"},config)
-          /*  const response = await fetch("https://pokemon-api-node.herokuapp.com/poke-api/v1/pokemon", {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                  'Content-Type': 'text/plain'
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: newPokemon // body data type must match "Content-Type" header
-              });*/
+           let response= await axios.post("https://pokemon-api-node.herokuapp.com/poke-api/v1/pokemon",data,config)
               alert(response)
         } catch (error) {
             alert(error)
